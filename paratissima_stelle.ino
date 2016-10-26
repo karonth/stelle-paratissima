6ba950f80a70b1f12d43888e60b1d72dcdb68176
Enkel Bici <ebici@dnshosting.local>
2016-10-26 22:55:16 +0200

    Added analog microphone on input A0 that substitutes the dist random generator

--- paratissima_stelle.ino
+++ paratissima_stelle.ino
@@ -1,6 +1,6 @@
 /*
 Lights display for paratissima
-
+@author Enkel Bici
 Based on inoise8_pal_demo code from Andrew Tuline
 */
 
@@ -9,7 +9,7 @@ Based on inoise8_pal_demo code from Andrew Tuline
 /*
 There are 21 stars with 8 leds each totalling 168
 */
-#define STARS 21
+#define STARS 19
 #define STAR_LEDS 8
 #define NUM_LEDS STARS*STAR_LEDS
 //#define NUM_LEDS 168
@@ -46,6 +46,8 @@ rgb(248,202,0)
 CRGB rgb(248,202,0);          //gold rgb
 static uint8_t baseHue = 48;  //gold hue
 
+// microphone on analog input 0
+static uint8_t audioInputPin = 0;
 
 CRGBPalette16 currentPalette(CRGB::Black);
 //CRGBPalette16 targetPalette(OceanColors_p);
@@ -56,17 +58,20 @@ void setup() {
   Serial.begin(115200);
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  //FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);  
 
   LEDS.setBrightness(BRIGHTNESS);
-  dist = random16(12345);          // A semi-random number for our noise generator
-
-  //FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);  
+  
+  //dist = random16(12345);          // A semi-random number for our noise generator
+  
 }
 
 void loop() { 
 
   //testYellow();
-  
+  //read analog microphone
+  dist=analogRead(audioInputPin);
+  Serial.println(dist);
 
   EVERY_N_MILLISECONDS(10) {
     nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);  // Blend towards the target palette
@@ -75,6 +80,7 @@ void loop() {
   
   EVERY_N_SECONDS(5) {             
   // Change the target palette to a random saturation every 5 seconds.
+
     targetPalette = CRGBPalette16(
       CHSV(baseHue, 255, random8(128,255)), 
       CHSV(baseHue, 255, random8(128,255)), 
@@ -87,26 +93,18 @@ void loop() {
   
 }
 
-
 void fillnoise8() {
+  //int seed = analogRead(0);
+  //dist = random16(seed);                                                   // A semi-random number for our noise generator
+
   for(int i = 0; i < NUM_LEDS; i++) {                                      // Just ONE loop to fill up the LED array as all of the pixels change.
     uint8_t index = inoise8(i*scale, dist+i*scale) % 255;                  // Get a value from the noise function. I'm using both x and y axis.
     leds[i] = ColorFromPalette(currentPalette, index, 255, LINEARBLEND);   // With that value, look up the 8 bit colour palette value and assign it to the current LED.
   }
   dist += beatsin8(10,1, 4);                                               // Moving along the distance (that random number we started out with). Vary it a bit with a sine wave.
                                                                            // In some sketches, I've used millis() instead of an incremented counter. Works a treat.
+
 } // fillnoise8()
-
-
-void SetupPaletteFromHue(int hue)
-{
-  targetPalette = CRGBPalette16( 
-                      CHSV( hue, 255, 32), 
-                      CHSV( hue, 255, 255), 
-                      CHSV( hue, 128, 255), 
-                      CHSV( hue, 255, 255)); 
-}
-
 
 
 void ledswitch(CRGB color){
